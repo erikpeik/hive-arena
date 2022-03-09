bba16b62eddd88f4b29ddd4d29f5fb3eef490e18
Erik Mende <emende@student.hive.fi>
2022-03-09 13:17:59 +0200

    Small changes in main.c and new thinks in .gitignore

--- example-agent-emende/main.c
+++ example-agent-emende/main.c
@@ -6,64 +6,73 @@
 
 int find_neighbour(agent_info_t info, cell_t type)
 {
-    coords_t center = {VIEW_DISTANCE, VIEW_DISTANCE};
+	coords_t center = {VIEW_DISTANCE, VIEW_DISTANCE};
 
-    for(int dir = 0 ; dir < 8 ; dir++)
-    {
-        coords_t coords = direction_to_coords(center, dir);
-        cell_t neighbour = info.cells[coords.row][coords.col];
-        if (neighbour == type)
-        {
-            return dir;
-        }
-    }
+	for(int dir = 0 ; dir < 8 ; dir++)
+	{
+		coords_t coords = direction_to_coords(center, dir);
+		cell_t neighbour = info.cells[coords.row][coords.col];
+		if (neighbour == type)
+		{
+			return dir;
+		}
+	}
 
-    return -1;
+	return -1;
 }
 
 command_t think(agent_info_t info)
 {
-    cell_t bee = info.cells[VIEW_DISTANCE][VIEW_DISTANCE];
+	static char	arr[NUM_ROWS][NUM_COLS];
 
-    if (is_bee_with_flower(bee))
-    {
-        int hive_dir = find_neighbour(info, hive_cell(info.player));
-        if (hive_dir >= 0)
-        {
-            return (command_t) {
-                .action = FORAGE,
-                .direction = hive_dir
-            };
-        }
-    }
-    else
-    {
-        int flower_dir = find_neighbour(info, FLOWER);
-        if (flower_dir >= 0)
-        {
-            return (command_t) {
-                .action = FORAGE,
-                .direction = flower_dir
-            };
-        }
-    }
+	cell_t bee = info.cells[VIEW_DISTANCE][VIEW_DISTANCE];
 
-    return (command_t) {
-        .action = MOVE,
-        .direction = rand() % 8
-    };
+	if (is_bee_with_flower(bee))
+	{
+		int hive_dir = find_neighbour(info, hive_cell(info.player));
+		if (hive_dir >= 0)
+		{
+			return (command_t) {
+				.action = FORAGE,
+				.direction = hive_dir
+			};
+		}
+		return (command_t) {
+			.action = MOVE,
+			.direction = rand() % 8
+		};
+	}
+	else
+	{
+		int flower_dir = find_neighbour(info, FLOWER);
+		if (flower_dir >= 0)
+		{
+			return (command_t) {
+				.action = FORAGE,
+				.direction = flower_dir
+			};
+		}
+		return (command_t) {
+			.action = MOVE,
+			.direction = rand() % 8
+		};
+	}
+/*	return (command_t) {
+		.action = MOVE,
+		.direction = rand() % 8
+	}; */
 }
 
 int main(int argc, char **argv)
 {
-    if (argc < 3)
-        panic("Usage: ./agent arena_host arena_ip");
+	if (argc < 3)
+		panic("Usage: ./agent arena_host arena_ip");
 
-    srand(time(NULL) + getpid());
+	srand(time(NULL) + getpid());
 
-    char *host = argv[1];
-    int port = atoi(argv[2]);
-    char *team_name = "example_agent";
+	char *host = argv[1];
+	int port = atoi(argv[2]);
+	char *team_name = "Eerikin Pippuri Kebab";
 
-    agent_main(host, port, team_name, think);
+	agent_main(host, port, team_name, think);
 }
