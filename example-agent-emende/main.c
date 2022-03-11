9f4f8f97dbfd9f106a4e986c73f371840f51e8d8
Erik Mende <emende@student.hive.fi>
2022-03-11 15:41:58 +0200

    Now bee's will try go different direction, if current direction is not EMPTY

--- example-agent-emende/main.c
+++ example-agent-emende/main.c
@@ -2,25 +2,65 @@
 
 int fd;
 
-coords_t locate_hive(int player, coords_t hive_loc)
+void locate_hive(int player, coords_t *hive_loc)
 {
 	if (player == 0)
 	{
-		hive_loc.row = (NUM_ROWS / 2);
-		hive_loc.col = 1;
+		hive_loc->row = (NUM_ROWS / 2);
+		hive_loc->col = 1;
 	}
 	else
 	{
-		hive_loc.row = (NUM_ROWS / 2);
-		hive_loc.col = (NUM_COLS - 2);
+		hive_loc->row = (NUM_ROWS / 2);
+		hive_loc->col = (NUM_COLS - 2);
 	}
-	return (hive_loc);
+}
+
+static const coords_t offsets[] = {
+    {-1, 0},
+    {-1, 1},
+    { 0, 1},
+    { 1, 1},
+    { 1, 0},
+    { 1,-1},
+    { 0,-1},
+    {-1,-1}
+};
+
+int	is_cell_free(agent_info_t info, int dir)
+{
+	int ofs = 1;
+	int temp;
+	coords_t offset = offsets[dir];
+	cell_t cell_info = info.cells[VIEW_DISTANCE + offset.row][VIEW_DISTANCE + offset.col];
+	if (cell_info == EMPTY)
+			return (dir);
+	while (ofs <= 2)
+	{
+		temp = dir;
+		if (dir + ofs > 7)
+			temp = -1 + ofs;
+		offset = offsets[temp + ofs];
+		cell_info = info.cells[VIEW_DISTANCE + offset.row][VIEW_DISTANCE + offset.col];
+		if (cell_info == EMPTY)
+			return (temp + ofs);
+		temp = dir;
+		if (dir - ofs < 0)
+			temp = 8 - ofs;
+		offset = offsets[temp - ofs];
+		cell_info = info.cells[VIEW_DISTANCE + offset.row][VIEW_DISTANCE + offset.col];
+		if (cell_info == EMPTY)
+			return (temp - ofs);
+		ofs++;
+	}
+	return (-1);
 }
 
 command_t think(agent_info_t info)
 {
 	static int	arr[NUM_ROWS][NUM_COLS];
 	int	flower_dir;
+	int rand_dir;
 	coords_t hive_loc;
 
 	/* Creating the map */
@@ -39,7 +79,7 @@ command_t think(agent_info_t info)
 	print_map(arr, fd, info);
 
 	/* Locate home Hive */
-	hive_loc = locate_hive(info.player, hive_loc);
+	locate_hive(info.player, &hive_loc);
 	if (is_bee_with_flower(bee))
 	{
 		int	hive_dir = find_neighbour(info, hive_cell(info.player));
@@ -51,9 +91,17 @@ command_t think(agent_info_t info)
 			};
 		}
 		hive_dir = return_to_hive(info, hive_loc);
+		hive_dir = is_cell_free(info, hive_dir);
+		if (hive_dir >= 0)
+			{
+			return (command_t) {
+				.action = MOVE,
+				.direction = hive_dir
+			};
+		}
 		return (command_t) {
 			.action = MOVE,
-			.direction = hive_dir
+			.direction = rand() % 8
 		};
 	}
 	else
@@ -67,6 +115,8 @@ command_t think(agent_info_t info)
 			};
 		}
 		flower_dir = find_distant(info, FLOWER);
+		if (flower_dir >= 0)
+			flower_dir = is_cell_free(info, flower_dir);
 		if (flower_dir >= 0)
 		{
 			return (command_t) {
