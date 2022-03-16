405fbdf54c3a4281954972e4a7b6a9313bab7c52
Erik Mende <emende@student.hive.fi>
2022-03-16 11:11:42 +0200

    Added total count, modified prints

--- script.sh
+++ script.sh
@@ -15,16 +15,18 @@ team1=$(basename $1)
 team2=$(basename $2)
 
 teamname1="EPK"
-teamname2="Wax builder"
+teamname2="Spy vs Spy"
 port=8000
 
 count1=0
 count2=0
+total_score1=0
+total_score2=0
 
 for (( c=0; c<$3; c++ ))
 do
 	isodate=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
-	logfile="${team1}-${team2}-${isodate}.log"
+	logfile="${team1}-${team2}-log-$c.log"
 
 	(sleep 0 ; cd $1 ; ./agent 127.0.0.1 $port | sed -e 's/^/[p0] /' ) & > /dev/null 2>&1
 	(sleep 0 ; cd $2 ; ./agent 127.0.0.1 $port | sed -e 's/^/[p1] /' ) & > /dev/null 2>&1
@@ -38,18 +40,22 @@ do
 		score2=$(tail -n2 $logfile | head -n1 | cut -d "," -f 4)
 		score1=$(tail -n2 $logfile | head -n1 | cut -d "," -f 5)
 	fi
-	printf "$teamname1 $score1 - $score2 $teamname2\n"
+	let "total_score1=total_score1+score1"
+	let "total_score2=total_score2+score2"
+	printf "GAME ${c}: $teamname1 $score1 - $score2 $teamname2\n"
 	if [ "$winner" == "$teamname1" ];
 	then
 		let "count1=count1+1"
 		echo "$teamname1 win!"
-	else
+	elsei
 		let "count2=count2+1"
 		echo "$teamname2 win!"
 	fi
 done
 printf "\nResults:\n"
-echo "$teamname1: $count1  -  $teamname2: $count2"
+echo "$teamname1 $count1  -  $teamname2 $count2"
+printf "Scores:\n"
+echo "$teamname1 $total_score1  -  $total_score2 $teamname2"
 printf "\nDo you want remove log files? (yes/no)\n"
 read remove_log
 if [ "$remove_log" == "yes" ]; then
