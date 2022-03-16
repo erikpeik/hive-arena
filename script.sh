31e2ff67f79ff0ca60d8a61dec08c27e35e93854
Erik Mende <emende@student.hive.fi>
2022-03-16 13:30:17 +0200

    More colors, much %%

--- script.sh
+++ script.sh
@@ -23,6 +23,15 @@ count2=0
 total_score1=0
 total_score2=0
 
+RED='\033[0;31m'
+LRED='\033[1;31m'
+LGREEN='\033[1;32m'
+LGRAY='\033[0;37m'
+LBLUE='\033[1;34m'
+YELLOW='\033[1;33m'
+NOCOL='\033[0m'
+
+
 for (( c=0; c<$3; c++ ))
 do
 	isodate=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
@@ -46,16 +55,34 @@ do
 	if [ "$winner" == "$teamname1" ];
 	then
 		let "count1=count1+1"
-		echo "$teamname1 win!"
-	else
+		printf "${LGREEN}$teamname1 win!${NOCOL}\n"
+	elif [ "$winner" == "$teamname2" ]; then
 		let "count2=count2+1"
-		echo "$teamname2 win!"
+		printf "${LRED}$teamname2 win!${NOCOL}\n"
+	else
+		printf "${YELLOW}DRAW${NOCOL}\n"
 	fi
 done
-printf "\nResults:\n"
-echo "$teamname1 $count1  -  $teamname2 $count2"
-printf "Scores:\n"
-echo "$teamname1 $total_score1  -  $total_score2 $teamname2"
+printf "\n${LBLUE}Results:${NOCOL}\n"
+echo "$teamname1	VS  $teamname2"
+echo "$count1	-  $count2"
+let "total_count=count1+count2"
+PERCENT1=$(echo "$count1/$total_count * 100" | bc -l)
+PERCENT2=$(echo "$count2/$total_count * 100" | bc -l)
+printf %.2f $PERCENT1
+printf "%%	-  "
+printf %.2f $PERCENT2
+printf "%%\n"
+printf "${LBLUE}Scores:${NOCOL}\n"
+echo "$teamname1	VS $teamname2"
+echo "$total_score1	-  $total_score2"
+let "total_max=total_score1+total_score2"
+PERCENT1=$(echo "$total_score1/$total_max * 100" | bc -l)
+PERCENT2=$(echo "$total_score2/$total_max * 100" | bc -l)
+printf %.2f $PERCENT1
+printf "%%	-  "
+printf %.2f $PERCENT2
+printf "%%\n"
 printf "\nDo you want remove log files? (yes/no)\n"
 read remove_log
 if [ "$remove_log" == "yes" ]; then
